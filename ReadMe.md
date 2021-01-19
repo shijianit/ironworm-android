385436d0ab424cf04b173bbf5cbb4e9b043ad7b6
shijian <shijian5@xiaomi.com>
2021-01-19 15:08:15 +0800

    修改apktool为2.4.1版本

---
+++ ReadMe.md
@@ -0,0 +1,17 @@
+##命令
+
+### 安装apktool
+```
+./apktool-install.sh
+```
+
+### 反编译apk
+需要将要反编译的apk，放到apks目录下
+```
+./apk-decode.sh
+```
+
+### 打包、签名、安装apk
+```
+./apk-build-and-install.sh
+```
--- apktool-install.sh
+++ apktool-install.sh
@@ -4,12 +4,12 @@ binApkTool="/usr/local/bin/apktool"
 binApkToolJar="/usr/local/bin/apktool.jar"
 
 if [ ! -f "$binApkTool" ]; then
-  sudo cp apktool/apktool /usr/local/bin/
+  sudo cp tools/apktool/apktool /usr/local/bin/
   sudo chmod 777 /usr/local/bin/apktool
 fi
 
 if [ ! -f "$binApkToolJar" ]; then
-  sudo cp apktool/apktool.jar /usr/local/bin/
+  sudo cp tools/apktool/apktool_2.4.1.jar /usr/local/bin/apktool.jar
   sudo chmod 777 /usr/local/bin/apktool.jar
 fi
 
